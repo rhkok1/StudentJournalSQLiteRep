0c557dd6ac4ad07c6fd9c8fa55a8f4c310f70de9
rhkok1 <romariopetruska47@gmail.com>
2026-06-08 08:18:08 +0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using SQLitePCL;
+using System.Windows;
+using StudentJournalSQLite.Database;
+using Microsoft.Data.Sqlite;
+
+namespace StudentJournalSQLite
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Batteries.Init();
+            Db.Initialize();
+            base.OnStartup(e);
+        }
+    }
+}
